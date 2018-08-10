a5862c1c9482026ced990aaaae8cbb087dbcea13
Chris Li <cli78822@gmail.com>
2018-08-10 04:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ function shout(str){
   return str.toUpperCase();
 }
 
-function whisper()
+function whisper(str){
+  return str.toLowerCase();
+}
+
+function logShout(str){
+  console.log(shout(str));
+}
+
+function logWhisper(str){
+  console.log(whisper(str));
+}
+
+function sayHiToGrandma(str){
+  if(str)
+}
